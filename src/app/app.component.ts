7d2302cc8d7ecf5f8c0e3575b36c5d4e2ff9030a
James Culbertson <info@turnkey-commerce.com>
2022-11-04 16:40:26 -0500

    Multi-page PDF based on ID tags.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,8 @@ export class AppComponent implements OnInit{
   // columns we will show on the table
   public displayedColumns = ['firstName', 'lastName', 'studentEmail', 'yearOfStudy', 'registrationNumber', 'course' ];
   //the source where we will get the data
-  public dataSource = new MatTableDataSource<Student>();
+  public dataSource1 = new MatTableDataSource<Student>();
+  public dataSource2 = new MatTableDataSource<Student>();
 
   //dependency injection
   constructor(private studentApiService: ApiService) {
@@ -33,23 +34,32 @@ export class AppComponent implements OnInit{
   getStudentsInformation() {
     this.studentApiService.getStudentsInformation()
       .subscribe((res)=>{
-        //console.log(res);
-        this.dataSource.data = res;
-        // this.dataSource.sort = this.sort;
-        // this.dataSource.paginator = this.paginator;
+        this.dataSource1.data = res;
+        this.dataSource2.data = res;
       })
   }
 
   generatePdf() {
-    html2canvas(document.querySelector('#report')!).then(canvas => {
-      // Adjust the image width and height to fit average report on page.
-      var imgWidth = 29.7;
-      var imgHeight = 21.0;
-      const contentDataURL = canvas.toDataURL('image/png');
-      let pdf = new jspdf('l', 'cm', 'letter'); //Generates PDF in landscape mode
-      //let pdf = new jspdf('p', 'cm', 'letter'); //Generates PDF in portrait mode
-      pdf.addImage(contentDataURL, 'PNG', 0, 0, imgWidth, imgHeight);  
-      pdf.save('report.pdf');   
-    }); 
+    // Put all of the tags that are reports in the following array...
+    const reportIdTags = ['#report1','#report2'];
+    let pdf = new jspdf('l', 'cm', 'letter'); //Generates PDF in landscape mode
+    // Adjust the report image width and height (cm) to fit average report on page.
+    var imgWidth = 28;
+    var imgHeight = 20.;
+    reportIdTags.forEach((tag, i) => {
+      html2canvas(document.querySelector(tag)!)
+      .then(canvas => {
+          const img = canvas.toDataURL('image/png')
+          pdf.addImage(img,'PNG', 1, 1, imgWidth, imgHeight);
+          if (reportIdTags.length > (i+1)) {
+              pdf.addPage()
+          }
+      })
+      .then(() => {
+          if ((i+1) === reportIdTags.length) {
+              pdf.save('report.pdf')
+          }
+      })
+    })
   }
 }
